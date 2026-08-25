fd1840d848d21eb8ae9e6bb788ab87b12c31d91c
Reaper <johngrimmreaper@disroot.org>
2026-08-25 20:46:17 +0000

    tclx: declare lexer for generated date parser

---
+++ micropolis-activity/src/tclx/src/tclxyacc.h
@@ -0,0 +1,12 @@
+/*
+ * tclxyacc.h --
+ *
+ * Declaration required by yacc-generated Extended Tcl parser code.
+ */
+
+#ifndef TCLX_YACC_H
+#define TCLX_YACC_H
+
+extern int yylex(void);
+
+#endif /* TCLX_YACC_H */
